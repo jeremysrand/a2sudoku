1a4299854ceeb33cdf283c5ca50a2fb51ddf5378
Jeremy Rand <jeremy@rand-family.com>
2015-07-15 08:23:05 -0500

    Initial Commit

---
+++ a2sudoku/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * a2sudoku
+ *
+ * Created by Jeremy Rand on 2015-07-15.
+ * Copyright (c) 2015 Jeremy Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
